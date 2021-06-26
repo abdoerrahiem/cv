7d90ea0620b26e2b9a2c7c1204ba990afab75cbd
Abdur Rahim <abdoerrahiem@gmail.com>
2021-06-27 06:52:33 +0700

    added splash screen

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.modal')
 const image = document.querySelector('.header__image')
 const closeModal = document.querySelector('.modal__close')
+const splash = document.querySelector('.splash')
 
 image.addEventListener('click', () => {
   modal.style.display = 'block'
@@ -8,4 +9,10 @@ image.addEventListener('click', () => {
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none'
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    splash.style.display = 'none'
+  }, 2500)
 })
